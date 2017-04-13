af08aba63f7fb64ce575064692c357625b8b91c4
adramani90 <adramani90@gmail.com>
2017-04-13 14:52:13 +0530

    initial commit

---
+++ Demo Repo/Demo Repo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo Repo
+//
+//  Created by Swami on 13/04/17.
+//  Copyright © 2017 brainstorm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
